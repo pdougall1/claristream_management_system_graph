6db30d4e8ca28b5f24715aaa6acc4ab0704e4f1f
Patrick Dougall <pdougall1@gmail.com>
2015-10-25 15:20:01 -0400

    Binds d3 elements to the graph object

--- src/graph.js
+++ src/graph.js
@@ -1,68 +1,92 @@
 var Graph = function (graphData, dimensions) {
+  this.graphData  = graphData;
+  this.dimensions = dimensions;
+
   // create the basic svg element everything will hag on
-  var stage = d3.select(dimensions.selector)
+  this.stage = d3.select(this.dimensions.selector)
     .append("svg")
-    .attr("width", dimensions.width)
-    .attr("height", dimensions.height)
+    .attr("width", this.dimensions.width)
+    .attr("height", this.dimensions.height)
 
-  // make some scales for the axes
-  var xScale = d3.scale.ordinal()
-    .rangeRoundBands([0, dimensions.innerWidth], .1)
-    .domain(graphData.rangeNames);
+  this.setScales = function () {
+    this.xScale = d3.scale.ordinal()
+      .rangeRoundBands([0, this.dimensions.innerWidth], .1)
+      .domain(this.graphData.rangeNames);
 
-  var yScale = d3.scale.linear()
-    .range([dimensions.topGraphHeight, dimensions.topMargin])
-    .domain([0, graphData.maxRange]);
+    this.yScale = d3.scale.linear()
+      .range([this.dimensions.topGraphHeight, this.dimensions.topMargin])
+      .domain([0, this.graphData.maxRange]);
+  }
 
-  // make some axes
-  var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom");
+  this.setAxes = function () {
+    this.xAxis = d3.svg.axis()
+      .scale(this.xScale)
+      .orient("bottom");
 
-  var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .orient("left");
+    this.yAxis = d3.svg.axis()
+      .scale(this.yScale)
+      .orient("left");
+  }
 
-  // add the axes
-  stage.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", "translate(" + dimensions.leftMargin + "," + (dimensions.topGraphHeight) + ")")
-    .attr("fill", '#7F8082')
-    .call(xAxis);
+  this.addXAxis = function () {
+    this.stage.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", "translate(" + this.dimensions.leftMargin + "," + (this.dimensions.topGraphHeight) + ")")
+      .attr("fill", '#7F8082')
+      .call(this.xAxis);
+  }
 
-  stage.append("g")
-    .attr("class", "y-axis")
-    .attr("transform", "translate(" + dimensions.leftMargin + ",0)")
-    .attr("fill", '#7F8082')
-    .call(yAxis);
+  this.addYAxis = function () {
+    this.stage.append("g")
+      .attr("class", "y-axis")
+      .attr("transform", "translate(" + this.dimensions.leftMargin + ",0)")
+      .attr("fill", '#7F8082')
+      .call(this.yAxis);
+  }
 
-  // add horizontal grid lines
-  stage.selectAll("line.horizontalGrid")
-    .data(yScale.ticks(graphData.maxRange))
-    .enter().append("line")
-    .attr("class", "horizontalGrid")
-    .attr("x1", dimensions.leftMargin)
-    .attr("x2", dimensions.leftGraphWidth)
-    .attr("y1", function(d){ return yScale(d);})
-    .attr("y2", function(d){ return yScale(d);})
-    .attr("stroke", "#DEDFE0")
-    .attr("shape-rendering", "crispEdges")
-    .attr("stroke-width", "1px");
+  this.addHorizontalGridLines = function () {
+    var yScale = this.yScale;
+    this.horizontalGridLines = this.stage.selectAll("line.horizontalGrid")
+      .data(yScale.ticks(graphData.maxRange))
+      .enter().append("line")
+      .attr("class", "horizontalGrid")
+      .attr("x1", this.dimensions.leftMargin)
+      .attr("x2", this.dimensions.leftGraphWidth)
+      .attr("y1", function(d){ return yScale(d);})
+      .attr("y2", function(d){ return yScale(d);})
+      .attr("stroke", "#DEDFE0")
+      .attr("shape-rendering", "crispEdges")
+      .attr("stroke-width", "1px");
+  }
 
-  // add ranges to the stage
-  var ranges = stage.selectAll(".range")
-    .data(graphData.parsedData)
-    .enter().append("g")
-    .attr("class", "g")
-    .attr("transform", function(d, i) { return "translate(" + xScale(i) + ",0)"; });
+  this.addRanges = function () {
+    var xScale = this.xScale;
+    this.ranges = this.stage.selectAll(".range")
+      .data(this.graphData.parsedData)
+      .enter().append("g")
+      .attr("class", "g")
+      .attr("transform", function(d, i) { return "translate(" + xScale(i) + ",0)"; });
+  }
 
-  // add statuses to each range
-  ranges.selectAll("rect")
-    .data(function (d) { return d.statuses })
-    .enter().append("rect")
-    .attr("width", xScale.rangeBand())
-    .attr("x", dimensions.leftMargin)
-    .attr("y", function(d) { return yScale(d.y1) })
-    .attr("height", function(d) { return yScale(d.y0) - yScale(d.y1) })
-    .style("fill", function(d) { return d.hex });
+  this.addStatusesToEachRange = function () {
+    var xScale = this.xScale;
+    var yScale = this.yScale;
+
+    this.ranges.selectAll("rect")
+      .data(function (d) { return d.statuses })
+      .enter().append("rect")
+      .attr("width", xScale.rangeBand())
+      .attr("x", dimensions.leftMargin)
+      .attr("y", function(d) { return yScale(d.y1) })
+      .attr("height", function(d) { return yScale(d.y0) - yScale(d.y1) })
+      .style("fill", function(d) { return d.hex });
+  }
+
+  this.setScales();
+  this.setAxes();
+  this.addXAxis();
+  this.addYAxis();
+  this.addHorizontalGridLines();
+  this.addRanges();
+  this.addStatusesToEachRange();
 }
